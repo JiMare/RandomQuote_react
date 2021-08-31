e3e72c6ce99222d3b73efed6e181fefa16351c89
JitkaM <zoa@seznam.cz>
2021-08-31 18:36:48 +0200

    random quote App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState('');
+
+  const randomColor = `rgb(${Math.round(Math.random() * 256)}, ${Math.round(Math.random() * 256)}, ${Math.round(Math.random() * 256)})`;
+
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes", {
+      method: "GET"
+   })
+   .then(response => response.json())
+   .then(data => { setQuotes(data); setQuote(data[Math.round(Math.random()* 1643)]) });
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{backgroundColor: randomColor}}>
+      <div id="quote-box">
+        <div id="text" style={{color: randomColor}}>{quote.text}</div>
+        <div id="author" style={{color: randomColor}}>- {quote.author ?? 'author unknown'}</div>
+        <button id="new-quote" onClick={() => setQuote(quotes[Math.round(Math.random()* 1643)])} style={{backgroundColor: randomColor}}>Next quote</button>
+      </div>
     </div>
   );
 }
